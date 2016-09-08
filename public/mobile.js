9fd49cd45722e22ff417a8bf07e21e0652067cf1
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-08 13:00:25 +0700

    Переписал js удаление, создание

--- public/mobile.js
+++ public/mobile.js
@@ -1,64 +1,75 @@
 var app = angular.module('myApp',[]);
 app.controller('myCtrl', function($scope, $http){
+	// переменныя
 	$scope.card = { 
 		title: '', 
 		content: '' 
 	};
 
+	// служебныя функции
+	$scope.displayResult = function(type, text=''){
+		var color = 'black';
+		switch(type){
+			case 'error':
+				color = 'red';
+				break;
+			case 'success':
+				color = 'green';
+				break;
+		}
+
+		$scope.resultMsgColor = color;
+		$scope.resultMsgText = text;
+	}
+
+	$scope.clearForm = function(){
+		$scope.card.title = '';
+		$scope.card.content = '';
+	}
+
+	// основныя функции
 	$scope.readData = function(){
 		$http.get("/items").then(function(response) {
 			$scope.list = response.data;
 		});
 	};
 
-	$scope.sendData = function(){
-			var request = new Object;
-					request.card_title = $scope.card.title;
-					request.card_content = $scope.card.content;
+	$scope.createItem = function(){
+			var request = {
+				"card_title": $scope.card.title,
+				"card_content": $scope.card.content
+			}
 
 			$http({
 				method: 'POST',
 				url: '/items/new',
 				data: request
-			}).then(function successCallback(response) {
-					$scope.resultMsg = response.status+' ('+response.statusText+') '+response.data.message;
-					$scope.card.title = '';
-					$scope.card.content = '';
-				}, function errorCallback(response) {
-					$scope.resultMsg = response.status+' ('+response.statusText+') '+response.data.error;
-			});
+			}).then(
+				function successCallback(response) {
+					$scope.clearForm();
+					$scope.displayResult('success','карточка создана');
+				},
+				function errorCallback(response) {
+					$scope.displayResult('error','ошибка создания карточки');
+				}
+			);
 
 			$scope.readData();
 	};
 
 	$scope.deleteItem = function(id){
-		//alert('deleteItem '+id);
+		var html_id = '#card'+id;
 
 		$http({
 			method: 'DELETE',
 			url: '/items/'+id
 		}).then(
 			function successCallback(response) {
-				var target_id = '#card'+id;
-
-				var msg = response.data.message;
-
-				if ('success'==msg.type) {
-					angular.element(target_id).remove();
-					var msg_prefix = 'УСПЕХ';
-					var msg_text = msg.text;
-				} else {  
-					var msg_prefix = 'ОШИБКА';
-					var msg_text = msg.text;
-				}
-
-				$scope.resultMsg = msg_prefix + ': ' + msg_text;
+				angular.element(html_id).remove();
+				$scope.displayResult('success',"карточка "+id+" удалена");
 			},
 			function errorCallback(response) {
-				var msg_prefix = 'ОШИБКА';
-				var msg_text = response.status+' ('+response.statusText+') '+response.data.error;
-
-				$scope.resultMsg = msg_prefix + ': ' + msg_text;
+				$scope.displayResult('success',"ошибка удаления карточки "+id);
 			}
 		);
 	};
