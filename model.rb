e083e54e52f15d16037d34c662cb99b156696743
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-20 20:53:42 +0700

    avatar в модели

--- model.rb
+++ model.rb
@@ -1,4 +1,21 @@
 class Item < ActiveRecord::Base
 	validates :title, presence: true
 	validates :content, presence: true
+
+	has_attached_file(
+		:avatar, 
+		styles: { medium: "300x300>", thumb: "80x80>" }, 
+		default_style: :medium,
+		default_url: 'missing_image.png',
+		use_timestamp: false,
+		path: 'public/images/:style/:hash.:extension',
+    	url: '/images/:style/:hash.:extension',
+    	hash_secret: 'really-long-random-generated-string-not-publicly-accessible',
+	)
+	validates_attachment(:avatar,
+		# 	presence: {message:'не может быть пустым'},
+			content_type: { content_type: /\Aimage\/.*\Z/ },
+			size: { in: 1..1024**2 },
+		# 	#if: "'картинка'==self.kind"
+	)
 end
