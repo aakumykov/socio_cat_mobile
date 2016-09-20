1de15006243ea62c8e025602da5e2764f0bf4036
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-20 21:52:47 +0700

    test.rb берёт настоящую модель

--- test.rb
+++ test.rb
@@ -4,19 +4,5 @@ require_relative 'config/application.rb'
 
 ActiveRecord::Base.establish_connection(adapter:'sqlite3', database: 'db/data.sqlite3')
 
-class Item < ActiveRecord::Base
-	include Paperclip::Glue
+require_relative 'model.rb'
 
-	validates :title, presence: true
-	validates :content, presence: true
-
-	has_attached_file(
-		:avatar, 
-		path: 'public/images/:style/:filename',
-		styles: { medium: "300x300>", thumb: "100x100>" }, 
-		default_style: :medium,
-		default_url: 'missing_image.png',
-		use_timestamp: true,
-	)	
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-end
