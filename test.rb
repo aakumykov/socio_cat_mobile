74fae83a6304fbe020ddc4ec502d7972cadb5454
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-20 20:54:39 +0700

    attachment в test.rb

--- test.rb
+++ test.rb
@@ -1,7 +1,22 @@
 require 'active_record'
+require 'paperclip'
 require 'faker'
+
 ActiveRecord::Base.establish_connection(adapter:'sqlite3', database: 'db/data.sqlite3')
+
 class Item < ActiveRecord::Base
+	include Paperclip::Glue
+
 	validates :title, presence: true
 	validates :content, presence: true
+
+	has_attached_file(
+		:avatar, 
+		path: 'public/images/:style/:filename',
+		styles: { medium: "300x300>", thumb: "100x100>" }, 
+		default_style: :medium,
+		default_url: 'missing_image.png',
+		use_timestamp: true,
+	)	
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
